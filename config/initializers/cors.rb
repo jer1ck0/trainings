abc0f87b75206f05222beb14a0f0606e9d33fd23
Алексей Лебедев <aleksejlebedev@MacBook-Air-Aleksej.local>
2025-01-05 16:13:06 +0300

    Изменена авторизация Вычищен проект

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,10 @@
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#     allow do
-#       origins '*' # Разрешить все домены, замените на конкретный домен при необходимости
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*' # Разрешить все домены, замените на конкретный домен при необходимости
   
-#       resource '*',
-#         headers: :any,
-#         methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#     end
-#   end
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
+  end
   
--- db/migrate/20240927191759_add_creator_id_and_base_to_exersize.rb
+++
@@ -1,8 +0,0 @@
-class AddCreatorIdAndBaseToExersize < ActiveRecord::Migration[7.2]
-  def change
-    add_column :exersizes, :base, :boolean
-    add_column :exersizes, :creator_id, :integer
-    add_column :exersizes, :base_exersize_id, :integer
-    add_column :exersizes, :support_type, :string
-  end
-end
