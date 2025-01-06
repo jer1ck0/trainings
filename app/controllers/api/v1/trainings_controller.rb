11684c69aa6972ce483e198474e1c996e8a235b0
Лебедев Алексей <lex.lebedev@gmail.com>
2025-01-06 23:45:27 +0300

    Добавлено редактирование и удаление тренировки

--- app/controllers/api/v1/trainings_controller.rb
+++ app/controllers/api/v1/trainings_controller.rb
@@ -10,15 +10,39 @@ module Api
       end
 
       def create
-        p 'create'
+        training = Training.new(user_id: current_user.id)
+        if training.save
+          render json: { training: training }, status: :created
+        end
+      end
+
+      def show
+        training = Training.for_user(current_user).find(params[:id])
+
+        if training
+          render json: { training: training }, status: :ok
+        end
       end
 
       def update
-        p 'update'
+        training = Training.for_user(current_user).find(params[:id])
+
+        if training.update(training_params)
+          render json: { training: training }, status: :ok
+        end
       end
 
       def destroy
-        p 'destroy'
+        training = Training.for_user(current_user).find(params[:id])
+        if training.destroy
+          render json: {}, status: :no_content
+        end
+      end
+
+      private
+
+      def training_params
+        params.require(:training).permit(:description)
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
-      resources :trainings, only: [:index, :create, :update, :destroy]
+      resources :trainings, only: [:index, :create, :update, :destroy, :show]
       post 'login', to: 'sessions#create'
     end
   end
